5d761d3c5195a83e650404e412d74c486779c0ea
jervincarlos <jervin@sliqpay.com>
2025-04-07 21:51:22 +0800

    Add navs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { HeaderComponent } from './shared/components/header/header.component';
   standalone: true,
   imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  isDarkMode = false;
+  isFocus = true;
 
-  toggleTheme(): void {
-    this.isDarkMode = !this.isDarkMode;
-    const themeClass = this.isDarkMode ? 'dark-theme' : '';
+  toggleTheme(isFocus = true): void {
+    this.isFocus = isFocus;
+    const themeClass = this.isFocus ? 'dark-theme' : '';
     document.documentElement.className = themeClass;
   }
 }
