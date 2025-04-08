924979b7c11adc64edd9c315fdc1530c0993dda1
jervincarlos <jervin@sliqpay.com>
2025-04-08 21:28:44 +0800

    Add sounds

--- src/app/shared/components/timer/timer.component.ts
+++ src/app/shared/components/timer/timer.component.ts
@@ -3,10 +3,11 @@ import {
   computed,
   effect,
   input,
+  OnInit,
   output,
   signal,
 } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -15,7 +16,7 @@ import { Subscription, interval } from 'rxjs';
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.scss',
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
   time = input(0);
   textColor = input('#4a90e2');
   hasFinished = output<boolean>();
@@ -23,6 +24,11 @@ export class TimerComponent {
   countdown = computed(() => this.formatTime(this.totalSeconds()));
   private totalSeconds = signal(this.time() * 60);
   private timerSubscription!: Subscription;
+  private startTime!: number;
+  private duration!: number; // store how long the timer should run (in seconds)
+  private startAudio = new Audio('start.mp3');
+  private finishAudio = new Audio('finish.mp3');
+  private stopAudio = new Audio('stop.mp3');
 
   isRunning = signal(false);
 
@@ -38,18 +44,29 @@ export class TimerComponent {
     );
   }
 
+  ngOnInit(): void {
+    this.startAudio.load();
+    this.finishAudio.load();
+    this.stopAudio.load();
+  }
+
   startCountdown(): void {
-    // const audio = new Audio();
-    // audio.src = 'assets/start.mp3';
-    // audio.load();
-    // audio.play();
+    this.startAudio.currentTime = 0;
+    this.startAudio.play();
 
     this.isRunning.set(true);
 
+    this.duration = this.totalSeconds();
+    this.startTime = Date.now();
+
     this.timerSubscription = interval(1000).subscribe(() => {
-      if (this.totalSeconds() > 0) {
-        this.totalSeconds.set(this.totalSeconds() - 1);
+      const elapsed = Math.floor((Date.now() - this.startTime) / 1000);
+      const remaining = this.duration - elapsed;
+
+      if (remaining > 0) {
+        this.totalSeconds.set(remaining);
       } else {
+        this.totalSeconds.set(0);
         this.stopCountdown(true);
         this.hasFinished.emit(true);
       }
@@ -57,10 +74,9 @@ export class TimerComponent {
   }
 
   stopCountdown(hasFinish = false): void {
-    // const audio = new Audio();
-    // audio.src = hasFinish ? 'assets/finish.mp3' : 'assets/stop.mp3';
-    // audio.load();
-    // audio.play();
+    const audio = hasFinish ? this.finishAudio : this.stopAudio;
+    audio.currentTime = 0;
+    audio.play();
 
     if (this.timerSubscription) {
       this.isRunning.set(false);
